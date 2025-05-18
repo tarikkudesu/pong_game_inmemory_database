23e0b0536694e85304a63d772550ff07e628f53f
tarikku_desu <amehritarik@gmail.com>
2025-05-18 15:32:09 +0100

    minor update

--- srcs/mdb.ts
+++ srcs/mdb.ts
@@ -1,5 +1,6 @@
 import crypto from 'crypto';
 import { WebSocket } from 'ws';
+import { Pong } from './game/index.js';
 
 export const invitationTimeout: number = 60;
 
@@ -31,11 +32,11 @@ export class Invitation {
 export class Player {
 	public username: string;
 	public img: string;
-	// public socket: WebSocket;
-	// constructor(username: string, img: string, socket: WebSocket) {
-	constructor(username: string, img: string) {
+	public socket: WebSocket;
+	// constructor(username: string, img: string) {
+	constructor(username: string, img: string, socket: WebSocket) {
 		this.username = username;
-		// this.socket = socket;
+		this.socket = socket;
 		this.img = img;
 	}
 }
@@ -43,10 +44,11 @@ export class Player {
 export class Room {
 	public player: Player;
 	public opponent: Player;
-	// public pong: Pong;
+	public pong: Pong;
 	public playerScore: number = 0;
 	public opponentScore: number = 0;
 	constructor(player: Player, opponent: Player) {
+		this.pong = new Pong();
 		this.player = player;
 		this.opponent = opponent;
 	}
@@ -54,13 +56,28 @@ export class Room {
 
 class Mdb {
 	private invitations: Map<string, Invitation> = new Map();
-	public playersOnline: Map<string, Player> = new Map();
+	private players: Map<string, Player> = new Map();
 	private rooms: Map<string, Room> = new Map();
 
 	constructor() {}
 	/*****************************************************************************************************************
 	 *                                           PLAYER ROOMS MANIPULATION                                           *
 	 *****************************************************************************************************************/
+
+	// * add room
+	// TODO: CLIENT UPDATE
+	addRoom(player: Player, opponent: Player): Room {
+		if (this.checkIfRoomExists(player.username, opponent.username)) throw new Error('room already exists');
+		const room: Room = new Room(player, opponent);
+		this.rooms.set(player.username + opponent.username, room);
+		return room;
+	}
+	// * remove room
+	// TODO: CLIENT UPDATE
+	removeRoom(player: string, opponent: string): void {
+		if (!this.checkIfRoomExists(player, opponent)) throw new Error('room does not exist');
+		this.rooms.delete(player + opponent);
+	}
 
 	// * check if room exists
 	checkIfRoomExists(player: string, opponent: string): boolean {
@@ -69,62 +86,42 @@ class Mdb {
 		return false;
 	}
 
-	// * check if player is in a room
-	checkIfPlayerIsInARoom(username: string): boolean {
-		const arr: Room[] = [...this.rooms.values()];
-		for (let i = 0; i < arr.length; i++) if (arr[i].player.username === username || arr[i].opponent.username === username) return true;
-		return false;
-	}
-
-	// * add room
-	addRoom(player: Player, opponent: Player): Room {
-		if (player.username === opponent.username) throw new Error('trying to play with yourself? clever huh!');
-		if (!this.checkIfPlayerExists(player.username)) throw new Error(player.username + ": player doesn't exist");
-		if (!this.checkIfPlayerExists(opponent.username)) throw new Error(opponent.username + ": player doesn't exist");
-		if (this.checkIfRoomExists(player.username, opponent.username)) throw new Error('room already exists');
-		const room: Room = new Room(player, opponent);
-		this.rooms.set(player.username + opponent.username, room);
-		return room;
-	}
-	// * remove room
-	removeRoom(player: string, opponent: string): void {
-		if (!this.checkIfRoomExists(player, opponent)) throw new Error('room does not exist');
-		this.rooms.delete(player + opponent);
-	}
-
 	/****************************************************************************************************************
 	 *                                        PLAYERS TABLE MANIPULATION                                            *
 	 ****************************************************************************************************************/
-	// * check if Player exists
-	checkIfPlayerExists(username: string): boolean {
-		if (this.playersOnline.get(username)) return true;
-		return false;
-	}
-	checkIfInvitationExists(sender: string, recipient: string): boolean {
-		if (this.playersOnline.get(sender + recipient)) return true;
-		return false;
-	}
 	// * new player
-	addPlayer(username: string, img: string, socket?: WebSocket) {
+	// TODO: CLIENT UPDATE
+	addPlayer(username: string, img: string, socket: WebSocket) {
 		if (this.checkIfPlayerExists(username)) throw new Error('Player already exists');
 		const hash: string = generateHash(username);
-		this.playersOnline.set(username, new Player(username, img));
+		this.players.set(username, new Player(username, img, socket));
 		return hash;
 	}
 	// * remove player
+	// TODO: CLIENT UPDATE
 	removePlayer(username: string) {
-		this.playersOnline.delete(username);
+		this.players.delete(username);
+	}
+
+	// * check if Player exists
+	checkIfPlayerExists(username: string): boolean {
+		if (this.players.get(username)) return true;
+		return false;
+	}
+	checkIfInvitationExists(sender: string, recipient: string): boolean {
+		if (this.players.get(sender + recipient)) return true;
+		return false;
 	}
 	// * get player id
 	getPlayer(username: string): Player {
-		const player: Player | undefined = this.playersOnline.get(username);
+		const player: Player | undefined = this.players.get(username);
 		if (!player) throw new Error("Player doesn't exists");
 		return player;
 	}
 	// * select username + img player
 	getAllPlayers() {
 		console.warn('getAllPlayers() is not ideal. Use getAllOtherPlayersWithInviteStatus() instead.');
-		return [...this.playersOnline.values()].map((ele) => ({
+		return [...this.players.values()].map((ele) => ({
 			username: ele.username,
 			img: ele.img,
 		}));
@@ -132,7 +129,7 @@ class Mdb {
 	// * select username + img from all players but the user
 	getAllOtherPlayers(username: string) {
 		console.warn('getAllPlayers() is not ideal. Use getAllOtherPlayersWithInviteStatus() instead.');
-		return [...this.playersOnline.values()]
+		return [...this.players.values()]
 			.map((ele) => {
 				if (ele.username !== username)
 					return {
@@ -143,10 +140,9 @@ class Mdb {
 			})
 			.filter(Boolean);
 	}
-	// TODO: Add invitations status
 	getAllOtherPlayersWithInviteStatus(username: string) {
 		const player: Player = this.getPlayer(username);
-		return [...this.playersOnline.values()]
+		return [...this.players.values()]
 			.map((ele) => {
 				if (ele.username !== username) {
 					let invite_status: string = 'none';
@@ -168,7 +164,9 @@ class Mdb {
 	 *                                      INVITATIONS TABLE MANIPULATION                                          *
 	 ****************************************************************************************************************/
 	// ? invite_status manipulation queries
+
 	// * create invitation
+	// TODO: CLIENT UPDATE
 	createInvitation(sender: string, recipient: string): void {
 		const sen: Player = this.getPlayer(sender);
 		const rec: Player = this.getPlayer(recipient);
@@ -176,6 +174,66 @@ class Mdb {
 		if (this.invitations.get(sen.username + rec.username))
 			throw new Error('stop trying to send invitation to this player, he already got one from you');
 		this.invitations.set(sen.username + rec.username, new Invitation(sen.username, rec.username, rec.img));
+	}
+	// * update accepted invitation
+	// TODO: CLIENT UPDATE
+	acceptInvitation(sender: string, recipient: string): void {
+		const sen: Player = this.getPlayer(sender);
+		const rec: Player = this.getPlayer(recipient);
+		if (sen.username === rec.username) throw new Error('you are trying to accept an invite to yourself, pretty smart huh!!');
+		const invite: Invitation = this.getInvitation(sen, rec);
+		if (invite.invite_status === 'pending') invite.invite_status = 'accepted';
+	}
+	// * update declined invitation
+	// TODO: CLIENT UPDATE
+	declineInvitation(sender: string, recipient: string): void {
+		const sen: Player = this.getPlayer(sender);
+		const rec: Player = this.getPlayer(recipient);
+		if (sen.username === rec.username) throw new Error('you are trying to accept an invite to yourself, pretty smart huh!!');
+		const invite: Invitation = this.getInvitation(sen, rec);
+		if (invite.invite_status === 'pending') invite.invite_status = 'declined';
+	}
+	// * delete all expired invitation
+	// TODO: CLIENT UPDATE
+	deleteExpiredInvitations() {
+		console.log([...this.invitations.values()]);
+		[...this.invitations.values()].forEach((ele) => {
+			if (Date.now() - ele.created_at > invitationTimeout) this.invitations.delete(ele.sender + ele.recipient);
+		});
+		console.log([...this.invitations.values()]);
+	}
+	// * cancel invitation
+	// TODO: CLIENT UPDATE
+	cancelInvitation(sender: string, recipient: string): void {
+		const sen: Player = this.getPlayer(sender);
+		const rec: Player = this.getPlayer(recipient);
+		this.invitations.delete(sen.username + rec.username);
+	}
+	// * cancel all player invitations
+	// TODO: CLIENT UPDATE
+	cancelAllPlayerInvitations(sender: string) {
+		this.getAllPlayerInvitations(sender).forEach((invite) => {
+			this.invitations.delete(sender + invite?.recipient);
+		});
+	}
+	// * delete a rejected invitation for a specific user
+	// TODO: CLIENT UPDATE
+	deleteRejectedInvitation(sender: string, recipient: string) {
+		this.cancelInvitation(sender, recipient);
+	}
+	// * delete all rejected invitation for a specific user
+	// TODO: CLIENT UPDATE
+	deleteAllRejectedInvitations(sender: string): void {
+		[...this.invitations.values()].forEach((invite) => {
+			if (invite.sender === sender && invite.invite_status === 'declined') {
+				this.invitations.delete(invite.sender + invite.recipient);
+			}
+		});
+	}
+	// ! NOT IDEAL
+	deleteAllInvitations(): void {
+		console.warn("Deleting all invitations, this isn' a nice thing to do my friend, are you depressed or what");
+		this.invitations.clear();
 	}
 	getAllInvitations() {
 		return [...this.invitations.values()].map((ele) => ({
@@ -205,58 +263,7 @@ class Mdb {
 		if (!invite) throw new Error(sender.username + ', ' + recipient.username + ': no such invitation');
 		return invite;
 	}
-	// * update accepted invitation
-	acceptInvitation(sender: string, recipient: string): void {
-		const sen: Player = this.getPlayer(sender);
-		const rec: Player = this.getPlayer(recipient);
-		if (sen.username === rec.username) throw new Error('you are trying to accept an invite to yourself, pretty smart huh!!');
-		const invite: Invitation = this.getInvitation(sen, rec);
-		if (invite.invite_status === 'pending') invite.invite_status = 'accepted';
-	}
-	// * update declined invitation
-	declineInvitation(sender: string, recipient: string): void {
-		const sen: Player = this.getPlayer(sender);
-		const rec: Player = this.getPlayer(recipient);
-		if (sen.username === rec.username) throw new Error('you are trying to accept an invite to yourself, pretty smart huh!!');
-		const invite: Invitation = this.getInvitation(sen, rec);
-		if (invite.invite_status === 'pending') invite.invite_status = 'declined';
-	}
-	// * delete all expired invitation
-	deleteExpiredInvitations() {
-		console.log([...this.invitations.values()]);
-		[...this.invitations.values()].forEach((ele) => {
-			if (Date.now() - ele.created_at > invitationTimeout) this.invitations.delete(ele.sender + ele.recipient);
-		});
-		console.log([...this.invitations.values()]);
-	}
-	// * cancel invitation
-	cancelInvitation(sender: string, recipient: string): void {
-		const sen: Player = this.getPlayer(sender);
-		const rec: Player = this.getPlayer(recipient);
-		this.invitations.delete(sen.username + rec.username);
-	}
-	// * cancel all player invitations
-	cancelAllPlayerInvitations(sender: string) {
-		this.getAllPlayerInvitations(sender).forEach((invite) => {
-			this.invitations.delete(sender + invite?.recipient);
-		});
-	}
-	// * delete a rejected invitation for a specific user
-	deleteRejectedInvitation(sender: string, recipient: string) {
-		this.cancelInvitation(sender, recipient);
-	}
-	// * delete all rejected invitation for a specific user
-	deleteAllRejectedInvitations(sender: string): void {
-		[...this.invitations.values()].forEach((invite) => {
-			if (invite.sender === sender && invite.invite_status === 'declined') {
-				this.invitations.delete(invite.sender + invite.recipient);
-			}
-		});
-	}
-	deleteAllInvitations(): void {
-		console.warn("Deleting all invitations, this isn' a nice thing to do my friend, are you depressed or what");
-		this.invitations.clear();
-	}
+	// TODO: everyupdate to the database should be followed by a client update
 }
 
 export const mdb = new Mdb();
